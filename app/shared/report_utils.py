b6a3bf86b7bc3aca4ed367037e54c87c775a2e80
Will <dev.willct@gmail.com>
2020-11-19 17:47:40 -0500

    Cambio de separador en produccion

--- app/shared/report_utils.py
+++ app/shared/report_utils.py
@@ -3,10 +3,16 @@ from weasyprint import HTML, CSS
 from django.template.loader import get_template
 from django.http import HttpResponse
 
+# separador en localhost
+# separador = '\\'
+# separador en producción
+separador = '/'
+
 
 def generar_reporte(source, data):
     html_template = get_template(source + '.html').render(data)
-    pdf_file = HTML(string=html_template).render(stylesheets=[CSS(STATICFILES_DIRS[0] +'\\css\\' + source + '.css')]).write_pdf()
+    pdf_file = HTML(string=html_template).render(stylesheets=[CSS(
+        STATICFILES_DIRS[0] + separador + 'css'+separador + source + '.css')]).write_pdf()
     response = HttpResponse(pdf_file, content_type='application/pdf')
-    response['Content-Disposition'] = 'filename="' + source +'.pdf"'
+    response['Content-Disposition'] = 'filename="' + source + '.pdf"'
     return response

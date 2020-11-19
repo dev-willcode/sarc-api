b86bcc7b80e53295ce7558607482ff9c6709051b
Will <dev.willct@gmail.com>
2020-11-19 17:43:03 -0500

    Corrección de archivos estáticos

--- app/shared/report_utils.py
+++ app/shared/report_utils.py
@@ -1,4 +1,4 @@
-from sarcapi.settings import TEMPLATE_DIR
+from sarcapi.settings import STATICFILES_DIRS
 from weasyprint import HTML, CSS
 from django.template.loader import get_template
 from django.http import HttpResponse
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 def generar_reporte(source, data):
     html_template = get_template(source + '.html').render(data)
-    pdf_file = HTML(string=html_template).render(stylesheets=[CSS(TEMPLATE_DIR +'\\css\\' + source + '.css')]).write_pdf()
+    pdf_file = HTML(string=html_template).render(stylesheets=[CSS(STATICFILES_DIRS[0] +'\\css\\' + source + '.css')]).write_pdf()
     response = HttpResponse(pdf_file, content_type='application/pdf')
     response['Content-Disposition'] = 'filename="' + source +'.pdf"'
     return response
